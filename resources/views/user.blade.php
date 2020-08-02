afb8c4a684b78b544adbd9858cd1d91f20cb2dea
Aboubacar305 <obou305@gmail.com>
2020-08-02 10:55:52 +0000

    first commit

---
+++ resources/views/user.blade.php
@@ -0,0 +1,8 @@
+<h1>user account</h1>
+<form action="usercontroller" method="post">
+<input type="text" name="email">
+<br>
+<input type="password " name="password">
+<br>
+<button type="submit">submit</button>
+</form>
